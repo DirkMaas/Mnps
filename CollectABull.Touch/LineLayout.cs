10837a27dfddf1e728b9ec1273851a881b6aacfb
Dirk Maas <dirkavlmaas@gmail.com>
2014-08-14 14:38:14 -0600

    mostly code cleanup

--- CollectABull.Touch/LineLayout.cs
+++ CollectABull.Touch/LineLayout.cs
@@ -17,61 +17,11 @@ using Cirrious.MvvmCross.ViewModels;
 
 namespace SimpleCollectionView
 {
-#if false
-	public class MyDataSource<T> : UICollectionViewDataSource where T : MnpsCell
-	{
-//		static NSString animalCellId = new NSString("AnimalCell");
-		static NSString cellId = new NSString(typeof(T).Name);
-//		List<IAnimal> animals;
-		List<ITableVSearchCellData> _colors;
-//		T _t;
-
-		public MyDataSource()
-		{
-//			animals = new List<IAnimal>();
-			var repo = Mvx.Resolve<Repository3>();
-			_colors = repo.Colors;
-//			foreach (colorsV c in colors)
-//				animals.Add(new Color(c.key, c.color));
-
-			// TODO add our data
-//			for (int i = 0; i < 20; i++)
-//				animals.Add(new Monkey());
-		}
-		public int? GetDbKey(int i)
-		{
-			return _colors[i].key;
-		}
-
-		public override int NumberOfSections(UICollectionView collectionView)
-		{
-			return 1;
-		}
-		public override int GetItemsCount(UICollectionView collectionView, int section)
-		{
-			return _colors.Count;
-		}
-
-		public override UICollectionViewCell GetCell(UICollectionView collectionView, MonoTouch.Foundation.NSIndexPath indexPath)
-		{
-			var cell = (T)collectionView.DequeueReusableCell(cellId, indexPath);
-
-			var animal = animals[indexPath.Row];
-
-			cell.Image = animal.Image;
-//			animalCell.key = animal.key; here's how to save the db key
-
-			return cell;
-		}
-	}
-#endif
 
 	public class MyDataSource2<C, T> : UICollectionViewDataSource where C : MnpsCell // and T is a table name
 	{
-//		static NSString animalCellId = new NSString("AnimalCell");
 		static NSString cellId = new NSString(typeof(C).Name);
 		List<TouchCellData> animals;
-//		working here
 		public MyDataSource2()
 		{
 			animals = new List<TouchCellData>();
@@ -79,11 +29,6 @@ namespace SimpleCollectionView
 			List<ITableVSearchCellData> _colors = repo.GetVTable<T>();
 			foreach (ITableVSearchCellData c in _colors)
 				animals.Add(new TouchCellData(c.key, c.descrip, c.graphic));
-//				animals.Add(new Monkey());
-
-			// TODO add our data
-			//			for (int i = 0; i < 20; i++)
-			//				animals.Add(new Monkey());
 		}
 		public int? GetDbKey(int i)
 		{
@@ -113,9 +58,7 @@ namespace SimpleCollectionView
 	public class LineLayout : UICollectionViewFlowLayout
 	{
 		public const float ITEM_SIZE = 50.0f;
-//		public const int ACTIVE_DISTANCE = 200;
-//		public const float ZOOM_FACTOR = 0.3f;
-		
+
 		public LineLayout()
 		{	
 			HeaderReferenceSize = new System.Drawing.SizeF(0, 0);
@@ -125,121 +68,9 @@ namespace SimpleCollectionView
 			SectionInset = new UIEdgeInsets(0, 0, 0, 0);
 //			FooterReferenceSize = new System.Drawing.SizeF (0, 0),
 //			MinimumInteritemSpacing
-//			this.supp
-		}
-// This is used in this case to ensure the code for transformation applied to the centermost cell will be 
-// applied during scrolling
-//		public override bool ShouldInvalidateLayoutForBoundsChange(RectangleF newBounds)
-//		{
-//			return true;
-//		}
-
-
-//		this is for zooming the middle item
-//		public override UICollectionViewLayoutAttributes[] LayoutAttributesForElementsInRect(RectangleF rect)
-//		{
-//			var array = base.LayoutAttributesForElementsInRect(rect);
-//			var visibleRect = new RectangleF(CollectionView.ContentOffset, CollectionView.Bounds.Size);
-//						
-//			foreach (var attributes in array)
-//			{
-//				if (attributes.Frame.IntersectsWith(rect))
-//				{
-//					float distance = visibleRect.GetMidX() - attributes.Center.X;
-//					float normalizedDistance = distance / ACTIVE_DISTANCE;
-//					if (Math.Abs(distance) < ACTIVE_DISTANCE)
-//					{
-//						float zoom = 1 + ZOOM_FACTOR * (1 - Math.Abs(normalizedDistance));
-//						attributes.Transform3D = CATransform3D.MakeScale(zoom, zoom, 1.0f);
-//						attributes.ZIndex = 1;											
-//					}
-//				}
-//			}
-//			return array;
-//		}
-
-//		makes the centermost cell snap to the center of the UICollectionView as scrolling stops
-//		public override PointF TargetContentOffset (PointF proposedContentOffset, PointF scrollingVelocity)
-//		{
-//			float offSetAdjustment = float.MaxValue;
-//			float horizontalCenter = (float)(proposedContentOffset.X + (this.CollectionView.Bounds.Size.Width / 2.0));
-//			RectangleF targetRect = new RectangleF(proposedContentOffset.X, 0.0f, this.CollectionView.Bounds.Size.Width, this.CollectionView.Bounds.Size.Height);
-//			var array = base.LayoutAttributesForElementsInRect(targetRect);
-//			foreach (var layoutAttributes in array)
-//			{
-//				float itemHorizontalCenter = layoutAttributes.Center.X;
-//				if (Math.Abs(itemHorizontalCenter - horizontalCenter) < Math.Abs(offSetAdjustment))
-//					offSetAdjustment = itemHorizontalCenter - horizontalCenter;
-//			}
-//            return new PointF(proposedContentOffset.X + offSetAdjustment, proposedContentOffset.Y);
-//		}
-	}
-
-#if false
-//	[MvxViewFor(typeof(SearchViewModel))]
-	public class MyViewController<T> : UIViewController //MvxViewController
-	{
-		static NSString cellId = new NSString(typeof(T).Name);
-			//new NSString("AnimalCell");
-		private UICollectionView _cv;
-		public MyViewController(UICollectionView cv)
-		{
-			_cv = cv;
-			// not sure these 2 lines do anything
-			AutomaticallyAdjustsScrollViewInsets = true;
-//			EdgesForExtendedLayout = UIRectEdge.None;
-		}
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-
-			_cv.RegisterClassForCell(typeof(T), cellId);
 		}
 	}
 
-	// MyViewController2 may not be necessary anymore
-	public class MyViewController2 : UIViewController //MvxViewController
-	{
-//		static NSString cellId = new NSString(typeof(T).Name);
-		NSString cellId2;
-		Type _type;
-		//new NSString("AnimalCell");
-//		private UICollectionView _cv;
-		public MyViewController2(/*UICollectionView cv, Type t*/)
-		{
-//			_cv = cv;
-//			_type = t;
-//			cellId2 = new NSString(_type.Name);
-			// not sure these 2 lines do anything
-			AutomaticallyAdjustsScrollViewInsets = true;
-			//			EdgesForExtendedLayout = UIRectEdge.None;
-		}
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-/*
-			Type t;
-			switch (_cv.Tag)
-			{
-			case 1:
-				t = typeof(ColorCell);
-//				id = new NSString(typeof(ColorCell).Name);
-				break;
-			case 2:
-				t = typeof(FlowerShapeCell);
-//				id = new NSString(typeof(FlowerShapeCell).Name);
-				break;
-			default:
-				throw new NotImplementedException();
-			}
-//			NSString id = new NSString(t.Name);
-*/
-
-//			_cv.RegisterClassForCell(typeof(ColorCell), new NSString(typeof(ColorCell).Name));
-//			_cv.RegisterClassForCell(typeof(FlowerShapeCell), new NSString(typeof(FlowerShapeCell).Name));
-		}
-	}
-#endif
 
 	public class MyCollectionView<T> : UICollectionView // MvxCollectionView does not exist
 	{
@@ -251,45 +82,30 @@ namespace SimpleCollectionView
 			ContentSize = new SizeF(300f, 50f);
 			BackgroundColor = UIColor.White;
 		}
-//		private MyViewController<T> _vc;
-//		public MyViewController<T> vc
-//		{
-//			get { return _vc; }
-//			set { _vc = value; }
-//		}
 	}
 
 	public class MyDelegate<C, T> : UICollectionViewDelegate where C : MnpsCell
 	{
-		SearchView _vc;
-//		delegate void SetKey(int? i);
-//		int? _key;
 		Action<int?> SetKey;
-		public MyDelegate(SearchView sv, Action<int?> sk)
+		public MyDelegate(Action<int?> sk)
 		{
-			_vc = sv;
-//			_key = k;
 			SetKey = sk;
 		}
 
-//		int? _dbKey;
-//		public int? DbKey
-//		{
-//			get { return _dbKey; }
-//			set { _dbKey = value; }
-//		}
+		public void Clear()
+		{
+			SetKey(null);
+		}
 
 		public override bool ShouldSelectItem(UICollectionView collectionView, NSIndexPath indexPath)
 		{
-			Console.WriteLine("ShouldSelectItem {0}", indexPath);
+//			Console.WriteLine("ShouldSelectItem {0}", indexPath);
 			var cell = (C)collectionView.CellForItem(indexPath);
 			if (cell.Selected) // needed if collectionView.AllowsMultipleSelection = false
 			{
-				Console.WriteLine("cell.Selected = false");
+//				Console.WriteLine("cell.Selected = false");
 //				cell.Selected = false; seems to have no effect
 				collectionView.DeselectItem(indexPath, true);
-//				_vc.ColorKey = null;
-//				_key = null;
 				SetKey(null);
 				return false;
 			}
@@ -299,51 +115,29 @@ namespace SimpleCollectionView
 		// only called when collectionView.AllowsMultipleSelection = true
 		public override bool ShouldDeselectItem(UICollectionView collectionView, NSIndexPath indexPath)
 		{
-			Console.WriteLine("ShouldDeselectItem  {0}", indexPath);
-			return true;
+//			Console.WriteLine("ShouldDeselectItem  {0}", indexPath);
+			return true; // true is the default if the function is not implemented
 		}
 
 
 		public override void ItemSelected(UICollectionView collectionView, NSIndexPath indexPath)
 		{
-			Console.WriteLine("ItemSelected  {0}", indexPath);
+//			Console.WriteLine("ItemSelected  {0}", indexPath);
 			var ds = collectionView.DataSource as MyDataSource2<C, T>;
-//			DbKey = ds.GetDbKey(indexPath.Row);
-//			var cv = collectionView as MyCollectionView<C>;
-//			cv.vc.DbKey = ds.GetDbKey(indexPath.Row);
-//			_vc.ColorKey = ds.GetDbKey(indexPath.Row);
-//			_key = ds.GetDbKey(indexPath.Row);
 			SetKey(ds.GetDbKey(indexPath.Row));
 
-//			if (_vc.ColorKeyChanged != null) 
-//				_vc.ColorKeyChanged(this, EventArgs.Empty);
-
-
 //			this is how to get from a cell to a db key
-			NSIndexPath[] ip = collectionView.GetIndexPathsForSelectedItems();
-			foreach (NSIndexPath path in ip)
-			{
-				Console.WriteLine("path  {0}", path);
-//				var dbkey = collectionView.DataSource.animals[indexPath.Row].key;
-			}
+//			NSIndexPath[] ip = collectionView.GetIndexPathsForSelectedItems();
+//			foreach (NSIndexPath path in ip)
+//				Console.WriteLine("path  {0}", path);
 
 		}
 		public override void ItemDeselected(UICollectionView collectionView, NSIndexPath indexPath)
 		{
-//			DbKey = null;
-			Console.WriteLine("ItemDeselected  {0}", indexPath);
+//			Console.WriteLine("ItemDeselected  {0}", indexPath);
 		}
 	}
 
-
-
-
-//	public interface IAnimal
-//	{
-//		int DbKey { get; }
-//
-//		UIImage Image { get; }
-//	}
 
 	public class TouchCellData : ITableVSearchCellData
 	{
@@ -378,55 +172,6 @@ namespace SimpleCollectionView
 		}
 	}
 
-#if false
-	public class Color : IAnimal
-	{
-		string _color;
-		public Color(int k, string c)
-		{
-			DbKey = k;
-			_color = c;
-		}
-
-		int _dbKey;
-		public int DbKey
-		{
-			private set { _dbKey = value; }
-			get { return _dbKey; }
-		}
-		public UIImage Image
-		{
-			get
-			{
-				return UIImage.FromBundle(_color + ".png");
-			}
-		}
-	}
-	// bell, funnel, trumpet
-	public class FlowerShape : IAnimal
-	{
-		string _shape;
-		public FlowerShape(int k, string c)
-		{
-			DbKey = k;
-			_shape = c;
-		}
-
-		int _dbKey;
-		public int DbKey
-		{
-			private set { _dbKey = value; }
-			get { return _dbKey; }
-		}
-		public UIImage Image
-		{
-			get
-			{
-				return UIImage.FromBundle(_shape + ".png");
-			}
-		}
-	}
-#endif
 	public abstract class MnpsCell : /*MvxCollectionViewCell*/ UICollectionViewCell
 	{
 		protected UIImageView imageView;
