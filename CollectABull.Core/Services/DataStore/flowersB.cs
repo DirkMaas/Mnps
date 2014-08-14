d36d00fa8e7d887906797f15fecf3e862059db50
Dirk Maas <dirkavlmaas@gmail.com>
2014-08-14 14:10:35 -0600

    added 2 new columns

--- CollectABull.Core/Services/DataStore/flowersB.cs
+++ CollectABull.Core/Services/DataStore/flowersB.cs
@@ -16,18 +16,19 @@ namespace CollectABull.Core.Services.DataStore
 		public string species { get; set; }
 		public string subspecies { get; set; }
 		public int numPetals { get; set; }
-//		public string ImagePath { get; set; }
 		[ForeignKey(typeof(heightV))]
 		public int height { get; set; }
 		[ForeignKey(typeof(leafArrangementV))]
 		public int leafArrangement { get; set; }
 		[ForeignKey(typeof(flowerShapeV))]
 		public int flowerShape { get; set; }
-//		[ForeignKey(typeof(flower_fruitSeedShapeV))]
-//		public int fruit_seedShape { get; set; }
 		[ForeignKey(typeof(leafShapeV))]
 		public int leafShape { get; set; }
 		[ForeignKey(typeof(leaf_stemTextureV))]
 		public int leaf_stemTexture { get; set; }
+		[ForeignKey(typeof(leafMarginV))]
+		public int leafMargin { get; set; }
+		[ForeignKey(typeof(leafVenationV))]
+		public int leafVenation { get; set; }
 	}	
 }
